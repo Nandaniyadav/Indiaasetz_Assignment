eb531c48bc3ea618e7dde55c488c0bee09325a9a
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-12-17 11:37:47 +0530

    use of grid in mui

--- src/components/common/DetailsAbout/About.js
+++ src/components/common/DetailsAbout/About.js
@@ -1,27 +1,54 @@
-import * as React from 'react';
-import './About-style.css'
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import { Avatar, Toolbar } from '@mui/material';
+import * as React from "react";
+import "./About-style.css";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import { Avatar, Grid, Toolbar,Item} from "@mui/material";
+
 
 const About = () => {
   return (
-    <div className='aboutcontainer'>
-    <Card sx={{ minWidth: 345 }}>
-    <Toolbar sx={{border:'none'}}>
-    <Avatar className='aboutprofile' alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-    </Toolbar>
-    <Toolbar><div className='profileName'>Cameron Williamson</div></Toolbar>
-    <Toolbar>
-        <div className='aboutheading'>About</div>
-    </Toolbar>
-    <Toolbar>
-        <div className='about'>Global real etstae investor Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin</div>
-    </Toolbar>
-      <CardMedia/>
-    </Card>
-    </div>
-  )
-}
+    <div className="aboutcontainer">
+      <Card sx={{ minWidth: 345 }}>
+        <Toolbar>
+          <Avatar
+            className="aboutprofile"
+            alt="Remy Sharp"
+            src="/static/images/avatar/1.jpg"
+          />
+        </Toolbar>
+        <Toolbar>
+          <div className="profileName">Cameron Williamson</div>
+        </Toolbar>
+        <Toolbar>
+          <div className="aboutheading">About</div>
+        </Toolbar>
+        <Toolbar>
+          <div className="about">
+            Global real etstae investor Contrary to popular belief, Lorem Ipsum
+            is not simply random text. It has roots in a piece of classical
+            Latin Contrary to popular belief, Lorem Ipsum is not simply random
+            text. It has roots in a piece of classical Latin
+          </div>
+        </Toolbar>
 
-export default About
+        <CardMedia />
+      </Card>
+<Toolbar>
+    <div className="aboutheading">Personal Details</div>
+</Toolbar>
+<Card sx={{ minWidth: 345 }}>
+    <CardMedia/>
+</Card>
+{/* Create Grid */}
+<Grid container className="gridcontainer">
+<Grid xs={3} className="grids">Client Id</Grid>
+<Grid xs={3} className="grids">gvhjgvjh</Grid>
+<Grid xs={3} className="grids">gvhjgvjh</Grid>
+<Grid xs={3} className="grids">gvhjgvjh</Grid>
+</Grid>
+
+    </div>
+  );
+};
+
+export default About;
